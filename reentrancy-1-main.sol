465ae1e810ebec220b874375cac1be9076f895c0
Large Zhe <92359448+wangzhecodingfy@users.noreply.github.com>
2022-10-31 18:49:33 +0800

    Add files via upload

---
+++ reentrancy-1-main.sol
@@ -0,0 +1,33 @@
+// pragma solidity ^0.4.0;
+
+contract ReentrancyBenign {
+    uint8 counter = 0;
+
+    function bad0() public {
+        // (bool success,) = msg.sender.call("");
+        // if (!success) {
+        //     revert();
+        // }
+        uint8 last = counter;
+        bad2();
+        counter += 1;          counter += 2;
+    }
+
+    function bad2() public {
+    }
+}
+
+
+contract _MAIN_ {
+    ReentrancyBenign testContract;
+
+    constructor () public {
+        testContract = new ReentrancyBenign();
+        uint8 test = 0;
+        if (test > 0) {
+            testContract.bad0();
+        } else {
+            testContract.bad0();
+        }
+    }
+}
